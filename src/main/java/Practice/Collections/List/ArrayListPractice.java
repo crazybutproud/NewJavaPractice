e9f95e2f58ee495dc2f5d0ceefd10319b1209cf2
crazybutproud <ms.terebonk@mail.ru>
2024-06-04 17:12:21 +0300

    Add Iterator Practice

--- src/main/java/Practice/Collections/List/ArrayListPractice.java
+++ src/main/java/Practice/Collections/List/ArrayListPractice.java
@@ -8,8 +8,17 @@ public class ArrayListPractice {
         al.add("А");
         al.add("Б");
         al.add("В");
+        al.add(1,"Two");
         System.out.println(al);
+        System.out.println(al.get(0));
+        al.set(2,"replaced element"); // set = replace
+        al.remove(0);
+        System.out.println(al.contains("A"));
 
-        ArrayList<Integer> al2 = new ArrayList<>(200); //лучше прописывать начальную длину листа
+        ArrayList<String> al2 = new ArrayList<>(200); //лучше прописывать начальную длину листа
+        al2.addAll(al);
+        System.out.println(al2.indexOf("Б"));
+        System.out.println(al);
+        System.out.println(al.size());
     }
 }
