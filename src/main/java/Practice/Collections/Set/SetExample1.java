6c33522fbb15c7c628f2c00c20da7f85d11f631f
crazybutproud <ms.terebonk@mail.ru>
2024-06-21 21:39:31 +0300

    Add Set Practice

--- src/main/java/Practice/Collections/Set/SetExample1.java
+++ src/main/java/Practice/Collections/Set/SetExample1.java
@@ -1,4 +1,20 @@
 package Practice.Collections.Set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetExample1 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Lichi");
+        set.add("Chichi");
+        set.add("Baka");
+        set.add("Kasip");
+        set.add("Masynua");
+
+        System.out.println(set);
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+        System.out.println(set.contains("Misha"));
+    }
 }
