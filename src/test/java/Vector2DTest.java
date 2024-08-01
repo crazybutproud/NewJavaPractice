1a7acda7e5a32896dbf2c7c7cebcf96697cc966d
crazybutproud <ms.terebonk@mail.ru>
2024-08-01 23:48:34 +0300

    Add JUnit practice

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,24 +1,30 @@
 import Practice.JUnit.Vector2D;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
+
+    @BeforeClass
+    public static void CreateNewVector() {
+        v1 = new Vector2D();
+    }
     @Test
     public void newVectorShoudHaveZeroLenght() {
-        Vector2D v1 = new Vector2D(); // action
         //assertion
-        Assert.assertEquals(0,v1.lenght(),1e-9);
+        Assert.assertEquals(0,v1.lenght(),EPS);
     }
 
     @Test
     public void newVectorShoudHaveZeroX() {
-        Vector2D v1 = new Vector2D(); // action
-        Assert.assertEquals(0,v1.getX(),1e-9);
+        Assert.assertEquals(0,v1.getX(),EPS);
     }
 
     @Test
     public void newVectorShoudHaveZeroY() {
-        Vector2D v1 = new Vector2D(); // action
-        Assert.assertEquals(0,v1.getY(),1e-9);
+        Assert.assertEquals(0,v1.getY(),EPS);
     }
 }
