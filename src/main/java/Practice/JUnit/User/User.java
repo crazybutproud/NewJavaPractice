b2d4c481a00fdccd67e20fa8e3b15b6f956f08e6
crazybutproud <ms.terebonk@mail.ru>
2024-08-02 23:47:45 +0300

    Add JUnit User practice

--- src/main/java/Practice/JUnit/User/User.java
+++ src/main/java/Practice/JUnit/User/User.java
@@ -1,6 +1,8 @@
 package Practice.JUnit.User;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private int id;
@@ -11,10 +13,19 @@ public class User {
     private static int countId = 0;
 
     public User(int id, String name, int age, String sex) {
-        this.id = id;
+        if (allUsers == null){
+            allUsers = new HashMap<>();
+        }
+
         this.name = name;
         this.age = age;
         this.sex = sex;
+
+        if (!hasUser()){
+            countId++;
+            this.id = countId;
+            allUsers.put(id, this);
+        }
     }
 
     public int getId() {
@@ -31,5 +42,37 @@ public class User {
 
     public String getSex() {
         return sex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name) && Objects.equals(sex, user.sex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, sex);
+    }
+
+    private boolean hasUser(){
+        for (User user : allUsers.values()){
+            if (user.equals(this) && user.hashCode() == this.hashCode()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
     }
 }
